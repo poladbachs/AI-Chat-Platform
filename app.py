057eea1090659aa37769c7ed11787824ed303ea1
Polad <bakhip@usi.ch>
2025-02-25 01:21:51 +0100

    changed port to 5001

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def chat():
     return jsonify({"response": response, "lead_score": lead_score})
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5001)
